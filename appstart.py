e1be688b52cc2a9684f2ec9150236ca29d550a5b
weiruduan <wrd@putao.cn>
2017-05-17 17:51:03 +0800

    fork

--- appstart.py
+++ appstart.py
@@ -31,10 +31,10 @@ if settings['ipv4']:
 else:
     sockets = bind_sockets(8888)
 
-if not settings['debug']:
-    import tornado.process
-
-    tornado.process.fork_processes(0)  # 0 表示按 CPU 数目创建相应数目的子进程
+# if not settings['debug']:
+#     import tornado.process
+#
+#     tornado.process.fork_processes(0)  # 0 表示按 CPU 数目创建相应数目的子进程
 server = HTTPServer(app, xheaders=True)
 server.add_sockets(sockets)
 tornado.ioloop.IOLoop.current().start()
