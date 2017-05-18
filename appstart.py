16227cc990a900c00a872e730bf93a8173e222c8
weiruduan <wrd@putao.cn>
2017-05-18 15:18:40 +0800

    reload

--- appstart.py
+++ appstart.py
@@ -4,6 +4,7 @@
 import json
 from functools import partial
 
+import sys
 import tornado.escape
 import tornado.gen
 import tornado.httpclient
@@ -26,17 +27,19 @@ app = Application([
     url('.*', PageNotFoundHandler)
 ], **settings)
 
+port = int(sys.argv[1]) if len(sys.argv) > 1 else LISTEN_PORT
+
 if settings['ipv4']:
     import socket
 
-    sockets = bind_sockets(LISTEN_PORT, family=socket.AF_INET)
+    sockets = bind_sockets(port, family=socket.AF_INET)
 else:
-    sockets = bind_sockets(LISTEN_PORT)
+    sockets = bind_sockets(port)
 
-if not settings['debug']:
-    import tornado.process
-
-    tornado.process.fork_processes(0)  # 0 表示按 CPU 数目创建相应数目的子进程
+# if not settings['debug']:
+#     import tornado.process
+#
+#     tornado.process.fork_processes(0)  # 0 表示按 CPU 数目创建相应数目的子进程
 server = HTTPServer(app, xheaders=True)
 server.add_sockets(sockets)
 
--- reload.sh
+++ reload.sh
@@ -1,6 +1,8 @@
 #逐个启动MyWeb每个端口进程，不中断服务
-
-/usr/local/bin/supervisorctl  -c /root/works/spide_script/supervisor_spide.conf restart baidu_search;
+for i in {6..9}
+    do /usr/local/bin/supervisorctl -c /root/works/spide_script/supervisor_spide.conf restart baidu_search:baidu_search$1;
+done
+#/usr/local/bin/supervisorctl  -c /root/works/spide_script/supervisor_spide.conf restart baidu_search;
 
 #重新加载nginx的配置
 /usr/local/nginx/sbin/nginx -s reload;
