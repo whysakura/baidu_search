6b2d4ebece9fc411aa84492a821b7cbb6e68d0cd
weiruduan <wrd@putao.cn>
2017-05-18 15:31:34 +0800

    $i

--- reload.sh
+++ reload.sh
@@ -1,6 +1,6 @@
 #逐个启动MyWeb每个端口进程，不中断服务
 for i in {6..9}
-    do /usr/local/bin/supervisorctl -c /root/works/spide_script/supervisor_spide.conf restart baidu_search:baidu_search$1;
+    do /usr/local/bin/supervisorctl -c /root/works/spide_script/supervisor_spide.conf restart baidu_search:baidu_search$i;
 done
 #/usr/local/bin/supervisorctl  -c /root/works/spide_script/supervisor_spide.conf restart baidu_search;
 
